d25509c64414911689cf48581b1c589216b65eb9
EunOh <bdohhhhh@gmail.com>
2025-06-07 19:20:36 +0900

    [add]: home types

---
+++ src/features/home/model/home.type.ts
@@ -0,0 +1,11 @@
+import type { Tables } from "@/types/supabase";
+
+export type Record = Tables<"allrecords">;
+
+export interface GetRecordsParams {
+  page: number;
+  limit: number;
+  search: string;
+  sort: string;
+  order: string;
+}
