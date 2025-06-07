1ae91f888654a37f5b33c68d44624f4a8b135d68
EunOh <bdohhhhh@gmail.com>
2025-06-07 21:14:28 +0900

    [update]: home-checkbox

--- src/features/home/ui/home-checkbox.tsx
+++ src/features/home/ui/home-checkbox.tsx
@@ -2,6 +2,9 @@
 
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import type { CheckedState } from "@radix-ui/react-checkbox";
+import { useState } from "react";
+import { useHomeStore } from "../model/home.store";
 
 interface HomeCheckboxProps {
   label: string;
@@ -9,9 +12,21 @@ interface HomeCheckboxProps {
 }
 
 function HomeCheckbox({ label, id }: HomeCheckboxProps) {
+  const { category, setCategory } = useHomeStore();
+  const [checked, setChecked] = useState<CheckedState>(category.includes(id));
+
+  const handleChange = (checked: CheckedState) => {
+    setChecked(checked);
+    if (checked) {
+      setCategory([...category, id]);
+    } else {
+      setCategory(category.filter((c) => c !== id));
+    }
+  };
+
   return (
     <div className="flex items-center justify-center gap-1 min-w-16">
-      <Checkbox id={id} defaultChecked />
+      <Checkbox id={id} checked={checked} onCheckedChange={handleChange} />
       <Label htmlFor={id}>{label}</Label>
     </div>
   );
