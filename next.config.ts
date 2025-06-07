2acdea29339ddf44ef1dc667738cee8cbec667d5
EunOh <bdohhhhh@gmail.com>
2025-06-07 21:59:26 +0900

    [chore]: next.cofig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["htjzfdsizwacysfiwjcd.supabase.co"],
+    domains: ["htjzfdsizwacysfiwjcd.supabase.co", "placehold.co"],
+    dangerouslyAllowSVG: true,
   },
 };
 
