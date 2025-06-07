4b134ef36b343a5ce828e330c1429d51309466bd
EunOh <bdohhhhh@gmail.com>
2025-06-07 19:21:05 +0900

    [update]: axios libs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import axios, { type AxiosError, type AxiosResponse } from "axios";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const api = axios.create({
+  baseURL: "/",
+});
+
+// api.interceptors.request.use((config) => {
+//   if (typeof window !== 'undefined') {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.set('Authorization', `Bearer ${token}`);
+//     }
+//   }
+//   return config;
+// });
+
+api.interceptors.response.use(
+  <T>(response: AxiosResponse<T>): T => response.data,
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
