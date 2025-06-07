af5903a9932602bc8fab50bbe345ff927c1faef7
EunOh <bdohhhhh@gmail.com>
2025-06-07 21:59:13 +0900

    [add]: record template

---
+++ src/features/record/index.ts
@@ -0,0 +1,4 @@
+export * from "./apis/record.apis";
+export * from "./hooks/record.queries";
+export { default as RecordHeader } from "./ui/record-header";
+export { default as RecordImages } from "./ui/record-images";
---
+++ src/templates/record-template.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { RecordHeader, RecordImages, useRecordQuery } from "@/features/record";
+
+interface RecordTemplateProps {
+  slug: string;
+}
+
+function RecordTemplate({ slug }: RecordTemplateProps) {
+  const { data: record, isPending, error } = useRecordQuery(slug);
+
+  if (isPending) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <main className="flex flex-col h-svh px-2">
+      <RecordHeader record={record} />
+      <RecordImages recordImages={record.images} />
+    </main>
+  );
+}
+
+export default RecordTemplate;
