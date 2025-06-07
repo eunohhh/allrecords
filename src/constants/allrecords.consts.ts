9023afd82afa78823c05151187e783cf46c5b9ff
EunOh <bdohhhhh@gmail.com>
2025-06-07 21:13:11 +0900

    [update]: constants

--- src/constants/allrecords.consts.ts
+++ src/constants/allrecords.consts.ts
@@ -1,1 +1,16 @@
 export const QUERY_KEY_RECORDS = "records";
+
+export const CHECKBOX_CATEGORY = [
+  {
+    label: "일상",
+    id: "daily",
+  },
+  {
+    label: "호숲",
+    id: "hosoop",
+  },
+  {
+    label: "작업",
+    id: "work",
+  },
+] as const;
