8605f220ab9aa1ef246ff6b429adc6e436a70377
EunOh <bdohhhhh@gmail.com>
2025-06-07 21:59:02 +0900

    [add]: record images

---
+++ src/features/record/ui/record-images.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import type { Json } from "@/types/supabase";
+import Image from "next/image";
+import type { RecordImage } from "../model/record.type";
+
+interface RecordImagesProps {
+  recordImages: Json[] | null;
+}
+
+function RecordImages({ recordImages }: RecordImagesProps) {
+  console.log(recordImages);
+  return (
+    <div className="flex flex-col gap-4">
+      {recordImages?.map((image) =>
+        image && typeof image === "object" ? (
+          <div className="relative h-96 w-full" key={(image as RecordImage).id}>
+            <Image
+              src={(image as RecordImage).url}
+              alt={(image as RecordImage).desc}
+              fill
+              className="object-cover"
+            />
+          </div>
+        ) : null
+      )}
+    </div>
+  );
+}
+
+export default RecordImages;
