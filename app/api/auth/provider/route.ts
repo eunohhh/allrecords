ef6265ff5859b8f199a441942f35690904d8e723
EunOh <bdohhhhh@gmail.com>
2025-06-14 19:36:12 +0900

    [add]: auth provider

---
+++ app/api/auth/provider/route.ts
@@ -0,0 +1,41 @@
+import { PUBLIC_URL } from "@/constants/allrecords.consts";
+import { createClient } from "@/lib/supabase/server";
+import type { Provider } from "@supabase/supabase-js";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const provider = searchParams.get("provider");
+  const next = searchParams.get("next");
+
+  const supabase = await createClient();
+
+  const getURL = () => {
+    let url =
+      PUBLIC_URL ?? // Set this to your site URL in production env.
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.endsWith("/") ? url : `${url}/`;
+    return url;
+  };
+
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: provider as Provider,
+    options: {
+      redirectTo: `${getURL()}/api/auth/callback?next=${next}`,
+    },
+  });
+
+  if (error) {
+    return NextResponse.json({ error: error?.message }, { status: 401 });
+  }
+
+  if (data.url) {
+    return NextResponse.redirect(data.url);
+  }
+
+  return NextResponse.json({ error: "Login failed" }, { status: 401 });
+}
