9ba995666d1e3a8f8e1c043d63631f8b49f3d255
EunOh <bdohhhhh@gmail.com>
2025-06-15 07:08:25 +0900

    [update]: admin index

--- src/features/admin/index.ts
+++ src/features/admin/index.ts
@@ -1,4 +1,5 @@
 export { default as AdminDatatable } from "./ui/admin-datatable";
+export { default as AdminHeader } from "./ui/admin-header";
 export { default as AdminLoginButton } from "./ui/admin-login-button";
 export { default as AdminPagination } from "./ui/admin-pagination";
 export { default as AdminSidebar } from "./ui/admin-sidebar";
