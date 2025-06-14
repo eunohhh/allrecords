43d11e7eaf5708143cd5064a7c6ac64a2125335c
EunOh <bdohhhhh@gmail.com>
2025-06-14 19:35:40 +0900

    [update]: signin page

--- app/(admin)/signin/page.tsx
+++ app/(admin)/signin/page.tsx
@@ -1,5 +1,27 @@
-function SignInPage() {
-  return <div>SignInPage</div>;
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AdminLoginButton } from "@/features/admin";
+import { getUser } from "@/lib/server-utils";
+import { redirect } from "next/navigation";
+
+async function SignInPage() {
+  const user = await getUser();
+
+  const isAdmin =
+    user?.email === process.env.ADMIN_EMAIL_1 ||
+    user?.email === process.env.ADMIN_EMAIL_2;
+
+  if (user && isAdmin) redirect("/admin");
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <AdminLoginButton />
+      </CardContent>
+    </Card>
+  );
 }
 
 export default SignInPage;
