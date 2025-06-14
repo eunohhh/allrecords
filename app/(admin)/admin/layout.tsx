451b8069071598f0bc6bc7025c74236ece01a819
EunOh <bdohhhhh@gmail.com>
2025-06-15 07:07:35 +0900

    [update]: admin layout

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,11 +1,14 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AdminSidebar } from "@/features/admin";
+import { getUser } from "@/lib/server-utils";
 import type { PropsWithChildren } from "react";
 
-function AdminLayout({ children }: PropsWithChildren) {
+async function AdminLayout({ children }: PropsWithChildren) {
+  const user = await getUser();
+
   return (
     <SidebarProvider>
-      <AdminSidebar />
+      <AdminSidebar user={user} />
       <main className="flex h-svh w-full">
         <SidebarTrigger className="cursor-pointer" />
         {children}
