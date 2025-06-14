6a60411a97b47a52c781add34656f569d190f2eb
EunOh <bdohhhhh@gmail.com>
2025-06-14 18:01:02 +0900

    [add]: signin page

--- app/(admin)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-function AdminLayout({ children }: PropsWithChildren) {
-  return <section className="flex h-svh w-full">{children}</section>;
-}
-
-export default AdminLayout;
