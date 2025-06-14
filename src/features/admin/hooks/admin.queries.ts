ac3d1b41a7f67d1f228afe5db3290a5d161cf1fc
EunOh <bdohhhhh@gmail.com>
2025-06-15 07:07:55 +0900

    [update]: admin queries

--- src/features/admin/hooks/admin.queries.ts
+++ src/features/admin/hooks/admin.queries.ts
@@ -1,11 +1,58 @@
-import { QUERY_KEY_ADMIN_RECORDS } from "@/constants/allrecords.consts";
+import {
+  QUERY_KEY_ADMIN_RECORDS,
+  QUERY_KEY_USER,
+} from "@/constants/allrecords.consts";
 import type { Record, RecordsParams } from "@/types/allrecords.types";
-import { useQuery } from "@tanstack/react-query";
-import { getAdminRecords } from "../apis/admin.apis";
+import type { User } from "@supabase/supabase-js";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  deleteAdminRecords,
+  getAdminRecords,
+  getAdminUser,
+  postAdminRecords,
+  putAdminRecords,
+} from "../apis/admin.apis";
 
 export const useAdminRecordsQuery = (params: RecordsParams) => {
   return useQuery<Record[], Error>({
     queryKey: [QUERY_KEY_ADMIN_RECORDS],
     queryFn: () => getAdminRecords(params),
+  });
+};
+
+export const useAdminRecordsMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<Record[], Error, RecordsParams>({
+    mutationFn: (params) => postAdminRecords(params),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ADMIN_RECORDS] });
+    },
+  });
+};
+
+export const useAdminRecordsDeleteMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<Record[], Error, string>({
+    mutationFn: (id) => deleteAdminRecords(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ADMIN_RECORDS] });
+    },
+  });
+};
+
+export const useAdminRecordsPutMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<Record[], Error, { id: string; params: RecordsParams }>({
+    mutationFn: ({ id, params }) => putAdminRecords(id, params),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ADMIN_RECORDS] });
+    },
+  });
+};
+
+export const useAdminUserQuery = () => {
+  return useQuery<User, Error>({
+    queryKey: [QUERY_KEY_USER],
+    queryFn: () => getAdminUser(),
   });
 };
