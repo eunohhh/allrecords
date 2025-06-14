4060a74d280a51165c2eec8136ab3dcd2e8df5db
EunOh <bdohhhhh@gmail.com>
2025-06-14 21:11:13 +0900

    [update]: home index

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,6 +1,5 @@
 export * from "./apis/home.apis";
 export * from "./hooks/home.queries";
-export * from "./model/home.type";
 export { default as HomeCheckbox } from "./ui/home-checkbox";
 export { default as HomeFloating } from "./ui/home-floating";
 export { default as HomeGrid } from "./ui/home-grid";
