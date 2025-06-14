5f1bcfdc1a38fe4a8726711774d258137a77234f
EunOh <bdohhhhh@gmail.com>
2025-06-15 08:07:04 +0900

    [update]: admin stores

--- src/features/admin/model/admin.store.ts
+++ src/features/admin/model/admin.store.ts
@@ -1,11 +1,30 @@
+import type { Record } from "@/types/allrecords.types";
 import { create } from "zustand";
 
 interface AdminStore {
   category: string[];
   setCategory: (category: string[]) => void;
+  selectedItems: Record[];
+  setSelectedItems: (selectedItem: Record) => void;
+  deleteSelectedItems: (selectedItem: Record) => void;
+  isModalOpen: boolean;
+  setIsModalOpen: (isModalOpen: boolean) => void;
 }
 
 export const useAdminStore = create<AdminStore>((set) => ({
   category: ["daily", "hosoop", "work"],
   setCategory: (category) => set({ category }),
+  selectedItems: [],
+  setSelectedItems: (selectedItem) =>
+    set((state) => ({
+      selectedItems: [...state.selectedItems, selectedItem],
+    })),
+  deleteSelectedItems: (selectedItem) =>
+    set((state) => ({
+      selectedItems: state.selectedItems.filter(
+        (item) => item.id !== selectedItem.id
+      ),
+    })),
+  isModalOpen: false,
+  setIsModalOpen: (isModalOpen) => set({ isModalOpen }),
 }));
