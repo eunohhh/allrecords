0b5510731235acfcd9c00ae2331ed590c095db77
EunOh <bdohhhhh@gmail.com>
2025-06-14 19:37:45 +0900

    [add]: lib server utils

---
+++ src/lib/server-utils.ts
@@ -0,0 +1,12 @@
+import { createClient } from "./supabase/server";
+
+export async function getUser() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    return null;
+  }
+
+  return data.user;
+}
