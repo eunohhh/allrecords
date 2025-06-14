7c75580e8847ab8be78a5f36a32918943bc6f3c9
EunOh <bdohhhhh@gmail.com>
2025-06-14 21:10:56 +0900

    [update]: home queries

--- src/features/home/hooks/home.queries.ts
+++ src/features/home/hooks/home.queries.ts
@@ -1,7 +1,7 @@
 import { QUERY_KEY_RECORDS } from "@/constants/allrecords.consts";
+import type { GetRecordsParams, Record } from "@/types/allrecords.types";
 import { useQuery } from "@tanstack/react-query";
 import { getRecords } from "../apis/home.apis";
-import type { GetRecordsParams, Record } from "../model/home.type";
 
 export const useRecordsQuery = (params: GetRecordsParams) => {
   return useQuery<Record[], Error>({
