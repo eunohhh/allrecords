b86c234c6c3559bfae2880eeb2e9f298f9ea0429
EunOh <bdohhhhh@gmail.com>
2025-06-15 15:58:11 +0900

    [update]: admin hooks

--- src/features/admin/hooks/admin.queries.ts
+++ src/features/admin/hooks/admin.queries.ts
@@ -2,7 +2,11 @@ import {
   QUERY_KEY_ADMIN_RECORDS,
   QUERY_KEY_USER,
 } from "@/constants/allrecords.consts";
-import type { Record, RecordsParams } from "@/types/allrecords.types";
+import type {
+  Record,
+  RecordPost,
+  RecordsParams,
+} from "@/types/allrecords.types";
 import type { User } from "@supabase/supabase-js";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import {
@@ -22,8 +26,30 @@ export const useAdminRecordsQuery = (params: RecordsParams) => {
 
 export const useAdminRecordsMutation = () => {
   const queryClient = useQueryClient();
-  return useMutation<Record, Error, Record>({
-    mutationFn: (params) => postAdminRecords(params),
+  return useMutation<Record, Error, RecordPost>({
+    mutationFn: (data) => {
+      const formData = new FormData();
+      formData.append("title", data.title);
+      formData.append("description", data.description);
+      formData.append("category", data.category);
+      formData.append("slug", data.slug);
+      formData.append("created_at", data.created_at);
+      formData.append("updated_at", data.updated_at);
+
+      const imagesInfo = data.images.map((image) => ({
+        id: image.id,
+        description: image.description,
+      }));
+      formData.append("imagesInfo", JSON.stringify(imagesInfo));
+
+      data.images.forEach((image) => {
+        if (image.file) {
+          formData.append("images", image.file);
+        }
+      });
+
+      return postAdminRecords(formData);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ADMIN_RECORDS] });
     },
@@ -42,8 +68,8 @@ export const useAdminRecordsDeleteMutation = () => {
 
 export const useAdminRecordsPutMutation = () => {
   const queryClient = useQueryClient();
-  return useMutation<Record[], Error, { id: string; params: RecordsParams }>({
-    mutationFn: ({ id, params }) => putAdminRecords(id, params),
+  return useMutation<Record[], Error, { id: string; data: RecordPost }>({
+    mutationFn: ({ id, data }) => putAdminRecords(id, data),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY_ADMIN_RECORDS] });
     },
