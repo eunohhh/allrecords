c5ae598cdf865e77b04c94290af4bb36745fc80d
EunOh <bdohhhhh@gmail.com>
2025-06-15 15:57:51 +0900

    [update]: admin apis

--- src/features/admin/apis/admin.apis.ts
+++ src/features/admin/apis/admin.apis.ts
@@ -1,21 +1,25 @@
 import api from "@/lib/utils";
-import type { Record, RecordsParams } from "@/types/allrecords.types";
+import type {
+  Record,
+  RecordPost,
+  RecordsParams,
+} from "@/types/allrecords.types";
 import type { User } from "@supabase/supabase-js";
 
 export function getAdminRecords(params: RecordsParams) {
   return api.get<Record[], Record[]>("/api/allrecords", { params });
 }
 
-export function postAdminRecords(params: Record) {
-  return api.post<Record, Record>("/api/allrecords", params);
-}
+export const postAdminRecords = async (formData: FormData) => {
+  return api.post<Record, Record>("/api/allrecords", formData);
+};
 
-export function deleteAdminRecords(params: { ids: string[] }) {
+export const deleteAdminRecords = async (params: { ids: string[] }) => {
   return api.post<Record[], Record[]>("/api/allrecords/delete", params);
-}
+};
 
-export function putAdminRecords(id: string, params: RecordsParams) {
-  return api.put<Record[], Record[]>(`/api/allrecords/${id}`, params);
+export function putAdminRecords(id: string, data: RecordPost) {
+  return api.put<Record[], Record[]>(`/api/allrecords/${id}`, data);
 }
 
 export function getAdminUser() {
