edd6a55dbf2dd95d02716a86a9bb84e3df96087f
EunOh <bdohhhhh@gmail.com>
2025-06-15 17:43:15 +0900

    [update]: admin layout

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,25 +1,16 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
-import { AdminSidebar } from "@/features/admin";
 import { getUser } from "@/lib/server-utils";
-import dynamic from "next/dynamic";
+import AdminLayoutTemplate from "@/templates/admin-layout-template";
 import type { PropsWithChildren } from "react";
-const SidebarProvider = dynamic(() =>
-  import("@/components/ui/sidebar").then((mod) => mod.SidebarProvider)
-);
 
 async function AdminLayout({ children }: PropsWithChildren) {
   const user = await getUser();
 
   return (
-    <SidebarProvider>
-      <AdminSidebar user={user} />
-      <main className="flex h-svh w-full">
-        <SidebarTrigger className="cursor-pointer" />
-        {children}
-      </main>
+    <>
+      <AdminLayoutTemplate user={user}>{children}</AdminLayoutTemplate>
       <Toaster richColors position="top-center" />
-    </SidebarProvider>
+    </>
   );
 }
 
--- app/(admin)/admin/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Loading from "@/components/ui/loading";
-
-function LoadingPage() {
-  return <Loading />;
-}
-
-export default LoadingPage;
