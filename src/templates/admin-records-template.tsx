9bfccc73fabe666c9a472af2bb1e7056ea1acb1c
EunOh <bdohhhhh@gmail.com>
2025-06-15 17:46:46 +0900

    [update]: admin records template

--- src/templates/admin-records-template.tsx
+++ src/templates/admin-records-template.tsx
@@ -1,12 +1,32 @@
 "use client";
 
-import { AdminDatatable, AdminHeader } from "@/features/admin";
+import { AdminDatatable, AdminHeader, AdminModal } from "@/features/admin";
+import { useAdminStore } from "@/features/admin/model/admin.store";
+import { useEffect } from "react";
+import { useShallow } from "zustand/react/shallow";
 
 function AdminRecordsTemplate() {
+  const { selectedItem, isModalOpen, setIsModalOpen } = useAdminStore(
+    useShallow((state) => ({
+      selectedItem: state.selectedItem,
+      isModalOpen: state.isModalOpen,
+      setIsModalOpen: state.setIsModalOpen,
+    }))
+  );
+
+  useEffect(() => {
+    if (selectedItem) setIsModalOpen(true);
+  }, [selectedItem, setIsModalOpen]);
+
   return (
     <section className="w-full flex flex-col gap-2 pr-4">
       <AdminHeader />
       <AdminDatatable />
+      <AdminModal
+        record={selectedItem ?? undefined}
+        open={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+      />
     </section>
   );
 }
