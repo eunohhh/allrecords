b28ea9b25297275d869f56e14098ca6a7d3a3074
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:01:12 +0900

    [add]: admin about datatable

---
+++ src/features/admin/ui/admin-about-datatable.tsx
@@ -0,0 +1,217 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import Loading from "@/components/ui/loading";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  type ColumnFiltersState,
+  type RowSelectionState,
+  type SortingState,
+  type VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ChevronDown, Loader2 } from "lucide-react";
+import { parseAsInteger, useQueryState } from "nuqs";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { useAdminDescsQuery } from "../hooks/admin.queries";
+import { aboutColumns } from "../model/admin.columns";
+import { useAdminStore } from "../model/admin.store";
+import AdminPagination from "./admin-pagination";
+
+const switchColumnIdToKorean = (id: string) => {
+  switch (id) {
+    case "category":
+      return "카테고리";
+    case "title":
+      return "제목";
+    case "created_at":
+      return "생성일";
+    case "updated_at":
+      return "수정일";
+    default:
+      return id;
+  }
+};
+
+function AdminAboutDatatable() {
+  const [page, setPage] = useQueryState("page", parseAsInteger);
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
+  const { aboutCategory } = useAdminStore();
+  const {
+    data: aboutData,
+    isPending,
+    error,
+  } = useAdminDescsQuery({
+    category: aboutCategory.join(","),
+    page: page ?? 1,
+    limit: 10,
+    search: "",
+    sort: "created_at",
+    order: "desc",
+  });
+
+  const table = useReactTable({
+    data: aboutData ?? [],
+    columns: aboutColumns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  useEffect(() => {
+    if (page) return;
+    setPage(1);
+  }, [page, setPage]);
+
+  useEffect(() => {
+    if (error) {
+      console.error("Error fetching records:", error);
+      toast.error("글 목록 조회에 실패했습니다.");
+    }
+  }, [error]);
+
+  if (isPending) return <Loading type="partial" />;
+
+  return (
+    <div className="w-full">
+      <div className="flex items-center py-2">
+        <Input
+          placeholder="Filter titles..."
+          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("title")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm bg-white"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto bg-white">
+              열 선택 <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {switchColumnIdToKorean(column.id)}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="rounded-md border bg-white">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={aboutColumns.length}
+                  className="h-24 text-center"
+                >
+                  <div className="flex items-center justify-center">
+                    <Loader2 className="mr-3 size-5 animate-spin" />
+                  </div>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="text-muted-foreground flex-1 text-sm">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="space-x-2">
+          <AdminPagination
+            page={table.getState().pagination.pageIndex + 1}
+            limit={table.getState().pagination.pageSize}
+            total={table.getFilteredRowModel().rows.length}
+            path="/admin/about"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdminAboutDatatable;
