456a2e85401823e0e491e67dbe4ba27a4fe4d45f
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:01:22 +0900

    [add]: admin add button

---
+++ src/features/admin/ui/admin-add-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useAdminStore } from "../model/admin.store";
+
+interface AdminAddButtonProps {
+  type: "records" | "descs";
+}
+
+function AdminAddButton({ type }: AdminAddButtonProps) {
+  const { setIsModalOpen } = useAdminStore();
+
+  return (
+    <Button className="cursor-pointer" onClick={() => setIsModalOpen(true)}>
+      <Plus />
+      {type === "records" ? "글쓰기" : "설명추가"}
+    </Button>
+  );
+}
+
+export default AdminAddButton;
