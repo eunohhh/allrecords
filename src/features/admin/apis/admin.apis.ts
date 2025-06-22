8870a1af9ce5793d9fa99b2f2962704ef6d33369
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:00:22 +0900

    [update]: admin apis

--- src/features/admin/apis/admin.apis.ts
+++ src/features/admin/apis/admin.apis.ts
@@ -1,23 +1,44 @@
 import api from "@/lib/utils";
-import type { Record, RecordsParams } from "@/types/allrecords.types";
+import type {
+  Desc,
+  DescsParams,
+  Record,
+  RecordsParams,
+} from "@/types/allrecords.types";
 import type { User } from "@supabase/supabase-js";
 
 export function getAdminRecords(params: RecordsParams) {
   return api.get<Record[], Record[]>("/api/allrecords", { params });
 }
 
-export const postAdminRecords = async (formData: FormData) => {
+export function postAdminRecords(formData: FormData) {
   return api.post<Record, Record>("/api/allrecords", formData);
-};
+}
 
-export const deleteAdminRecords = async (params: { ids: string[] }) => {
+export function deleteAdminRecords(params: { ids: string[] }) {
   return api.post<Record[], Record[]>("/api/allrecords/delete", params);
-};
+}
 
 export function putAdminRecords(id: string, formData: FormData) {
   return api.put<Record[], Record[]>(`/api/allrecords/${id}`, formData);
 }
 
-export function getAdminUser() {
-  return api.get<User, User>("/api/admin/user");
+export function getUser() {
+  return api.get<User, User>("/api/auth/user");
+}
+
+export function getAdminDescs(params: DescsParams) {
+  return api.get<Desc[], Desc[]>("/api/descs", { params });
+}
+
+export function postAdminDescs(data: Desc) {
+  return api.post<Desc, Desc>("/api/descs", data);
+}
+
+export function deleteAdminDescs(params: { ids: string[] }) {
+  return api.post<Desc[], Desc[]>("/api/descs/delete", params);
+}
+
+export function putAdminDescs(id: string, data: Desc) {
+  return api.put<Desc[], Desc[]>(`/api/descs/${id}`, data);
 }
