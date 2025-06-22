6252c7be6de6d62725c9f0ae406177795398da15
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:00:49 +0900

    [update]: admin columns

--- src/features/admin/model/admin.columns.tsx
+++ src/features/admin/model/admin.columns.tsx
@@ -2,17 +2,26 @@
 
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
-import type { Record } from "@/types/allrecords.types";
+import type { Desc, Record } from "@/types/allrecords.types";
 import type { ColumnDef } from "@tanstack/react-table";
 import { format } from "date-fns";
 import { ArrowUpDown, Pencil, Trash2 } from "lucide-react";
-import { useAdminRecordsDeleteMutation } from "../hooks/admin.queries";
+import {
+  useAdminDescsDeleteMutation,
+  useAdminRecordsDeleteMutation,
+} from "../hooks/admin.queries";
 import { useAdminStore } from "./admin.store";
 
-const { setSelectedItems, deleteSelectedItems, setSelectedItem } =
-  useAdminStore.getState();
+const {
+  setSelectedItems,
+  deleteSelectedItems,
+  setSelectedItem,
+  setSelectedDescs,
+  deleteSelectedDescs,
+  setSelectedDesc,
+} = useAdminStore.getState();
 
-export const columns: ColumnDef<Record>[] = [
+export const recordsColumns: ColumnDef<Record>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -113,9 +122,7 @@ export const columns: ColumnDef<Record>[] = [
     enableHiding: false,
     cell: ({ row }) => {
       const record = row.original;
-
       const { mutate: deleteRecords } = useAdminRecordsDeleteMutation();
-
       const handleDeleteRecord = () => deleteRecords([record.id]);
 
       return (
@@ -132,6 +139,139 @@ export const columns: ColumnDef<Record>[] = [
             className="h-8 w-8 p-0 cursor-pointer"
             onClick={() => {
               setSelectedItem(record);
+            }}
+          >
+            <Pencil />
+          </Button>
+        </div>
+      );
+    },
+  },
+];
+
+export const aboutColumns: ColumnDef<Desc>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ table, row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => {
+          if (value) {
+            setSelectedDescs(row.original);
+          } else {
+            deleteSelectedDescs(row.original);
+          }
+          row.toggleSelected(!!value);
+        }}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "category",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          카테고리
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("category")}</div>
+    ),
+  },
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          제목
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("title")}</div>,
+  },
+  {
+    accessorKey: "created_at",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          생성일
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div>{format(row.getValue("created_at"), "yyyy-MM-dd HH:mm")}</div>
+    ),
+  },
+  {
+    accessorKey: "updated_at",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          수정일
+          <ArrowUpDown />
+        </Button>
+      );
+    },
+    cell: ({ row }) => (
+      <div>{format(row.getValue("updated_at"), "yyyy-MM-dd HH:mm")}</div>
+    ),
+  },
+  {
+    accessorKey: "is_select",
+    header: () => <div>선택</div>,
+    cell: ({ row }) => <div>{row.getValue("is_select") ? "O" : "X"}</div>,
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const record = row.original;
+      const { mutate: deleteDescs } = useAdminDescsDeleteMutation();
+      const handleDeleteDesc = () => deleteDescs([record.id]);
+
+      return (
+        <div>
+          <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 cursor-pointer"
+            onClick={handleDeleteDesc}
+          >
+            <Trash2 />
+          </Button>
+          <Button
+            variant="ghost"
+            className="h-8 w-8 p-0 cursor-pointer"
+            onClick={() => {
+              setSelectedDesc(record);
             }}
           >
             <Pencil />
