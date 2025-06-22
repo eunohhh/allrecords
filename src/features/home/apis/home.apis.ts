39b3a68a64efa5640d03cbd3395d0739658a786b
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:03:45 +0900

    [update]: home apis

--- src/features/home/apis/home.apis.ts
+++ src/features/home/apis/home.apis.ts
@@ -1,6 +1,10 @@
 import api from "@/lib/utils";
-import type { Record, RecordsParams } from "@/types/allrecords.types";
+import type { Desc, Record, RecordsParams } from "@/types/allrecords.types";
 
 export function getRecords(params: RecordsParams) {
   return api.get<Record[], Record[]>("/api/allrecords", { params });
+}
+
+export function getDescs() {
+  return api.get<Desc, Desc>("/api/about");
 }
