803597a0a5fadd6419130a3e64e3b719f7c1b2c1
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:00:57 +0900

    [update]: admin store

--- src/features/admin/model/admin.store.ts
+++ src/features/admin/model/admin.store.ts
@@ -1,14 +1,21 @@
-import type { Record } from "@/types/allrecords.types";
+import type { Desc, Record } from "@/types/allrecords.types";
 import { create } from "zustand";
 
 interface AdminStore {
   category: string[];
   setCategory: (category: string[]) => void;
+  aboutCategory: string[];
+  setAboutCategory: (aboutCategory: string[]) => void;
+  selectedDesc: Desc | null;
+  setSelectedDesc: (selectedDesc: Desc | null) => void;
+  selectedDescs: Desc[];
+  setSelectedDescs: (selectedDescs: Desc) => void;
   selectedItem: Record | null;
   setSelectedItem: (selectedItem: Record | null) => void;
   selectedItems: Record[];
   setSelectedItems: (selectedItem: Record) => void;
   deleteSelectedItems: (selectedItem: Record) => void;
+  deleteSelectedDescs: (selectedDesc: Desc) => void;
   isModalOpen: boolean;
   setIsModalOpen: (isModalOpen: boolean) => void;
 }
@@ -16,6 +23,15 @@ interface AdminStore {
 export const useAdminStore = create<AdminStore>((set) => ({
   category: ["daily", "hosoop", "work"],
   setCategory: (category) => set({ category }),
+  aboutCategory: ["about"],
+  setAboutCategory: (aboutCategory) => set({ aboutCategory }),
+  selectedDesc: null,
+  setSelectedDesc: (selectedDesc) => set({ selectedDesc }),
+  selectedDescs: [],
+  setSelectedDescs: (selectedDescs) =>
+    set((state) => ({
+      selectedDescs: [...state.selectedDescs, selectedDescs],
+    })),
   selectedItem: null,
   setSelectedItem: (selectedItem) => set({ selectedItem }),
   selectedItems: [],
@@ -27,6 +43,12 @@ export const useAdminStore = create<AdminStore>((set) => ({
     set((state) => ({
       selectedItems: state.selectedItems.filter(
         (item) => item.id !== selectedItem.id
+      ),
+    })),
+  deleteSelectedDescs: (selectedDesc) =>
+    set((state) => ({
+      selectedDescs: state.selectedDescs.filter(
+        (desc) => desc.id !== selectedDesc.id
       ),
     })),
   isModalOpen: false,
