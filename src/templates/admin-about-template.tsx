0e673a5d177170c7c0e386460fae408a90be72e7
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:04:04 +0900

    [update]: admin about template

--- src/templates/admin-about-template.tsx
+++ src/templates/admin-about-template.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import {
+  AdminAboutDatatable,
+  AdminDescModal,
+  AdminHeader,
+} from "@/features/admin";
+import { useAdminStore } from "@/features/admin/model/admin.store";
+import { useEffect } from "react";
+import { useShallow } from "zustand/react/shallow";
+
 function AdminAboutTemplate() {
-  return <div>admin-about-template</div>;
+  const { selectedDesc, isModalOpen, setIsModalOpen } = useAdminStore(
+    useShallow((state) => ({
+      selectedDesc: state.selectedDesc,
+      isModalOpen: state.isModalOpen,
+      setIsModalOpen: state.setIsModalOpen,
+    }))
+  );
+
+  useEffect(() => {
+    if (selectedDesc) setIsModalOpen(true);
+  }, [selectedDesc, setIsModalOpen]);
+
+  return (
+    <section className="w-full flex flex-col gap-2 pr-4">
+      <AdminHeader title="descs" />
+      <AdminAboutDatatable />
+      <AdminDescModal
+        open={isModalOpen}
+        setIsModalOpen={setIsModalOpen}
+        desc={selectedDesc ?? undefined}
+      />
+    </section>
+  );
 }
 
 export default AdminAboutTemplate;
