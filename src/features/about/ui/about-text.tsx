14a6fe12b38943acea706279ef0fc499877353c4
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:00:07 +0900

    [update]: about text

--- src/features/about/ui/about-text.tsx
+++ src/features/about/ui/about-text.tsx
@@ -1,15 +1,29 @@
 "use client";
 
+import Loading from "@/components/ui/loading";
+import { useAboutQuery } from "@/features/home/hooks/home.queries";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
 function AboutText() {
+  const { data: about, isPending, error } = useAboutQuery();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (error) {
+      toast.error("Error getting about:");
+      router.push("/");
+    }
+  }, [error, router]);
+
   return (
     <div className="flex flex-col gap-4 pb-4">
-      <p className="text-xl font-bold">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-      </p>
+      {isPending ? (
+        <Loading type="partial" />
+      ) : (
+        <p className="text-xl font-bold">{about?.desc}</p>
+      )}
     </div>
   );
 }
