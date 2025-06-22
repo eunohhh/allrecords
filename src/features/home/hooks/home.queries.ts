bfdfd93e0b16f948de64c5ad099fdf4df4c82c3c
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:03:55 +0900

    [update]: home queries

--- src/features/home/hooks/home.queries.ts
+++ src/features/home/hooks/home.queries.ts
@@ -1,11 +1,19 @@
 import { QUERY_KEY_RECORDS } from "@/constants/allrecords.consts";
-import type { Record, RecordsParams } from "@/types/allrecords.types";
+import type { Desc, Record, RecordsParams } from "@/types/allrecords.types";
 import { useQuery } from "@tanstack/react-query";
-import { getRecords } from "../apis/home.apis";
+import { getDescs, getRecords } from "../apis/home.apis";
+import { QUERY_KEY_DESC } from "./../../../constants/allrecords.consts";
 
 export const useRecordsQuery = (params: RecordsParams) => {
   return useQuery<Record[], Error>({
     queryKey: [QUERY_KEY_RECORDS, params.category],
     queryFn: () => getRecords(params),
+  });
+};
+
+export const useAboutQuery = () => {
+  return useQuery<Desc, Error>({
+    queryKey: [QUERY_KEY_DESC],
+    queryFn: () => getDescs(),
   });
 };
