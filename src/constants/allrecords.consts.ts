a5ec07e4b4b11575a4f5e1dbce3470f34afb156c
EunOh <bdohhhhh@gmail.com>
2025-06-22 21:59:53 +0900

    [update]: consts

--- src/constants/allrecords.consts.ts
+++ src/constants/allrecords.consts.ts
@@ -1,7 +1,10 @@
 export const QUERY_KEY_RECORDS = "records";
 export const QUERY_KEY_RECORD = "record";
+export const QUERY_KEY_DESCS = "descs";
+export const QUERY_KEY_DESC = "desc";
 export const QUERY_KEY_USER = "user";
 export const QUERY_KEY_ADMIN_RECORDS = "admin-records";
+export const QUERY_KEY_ADMIN_DESCS = "admin-descs";
 
 export const CHECKBOX_CATEGORY = [
   {
