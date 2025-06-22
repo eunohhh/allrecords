39bd4f3eebad4228ec8e8c3caa27f97ec6db67e9
EunOh <bdohhhhh@gmail.com>
2025-06-22 22:02:03 +0900

    [update]: admin header

--- src/features/admin/ui/admin-header.tsx
+++ src/features/admin/ui/admin-header.tsx
@@ -1,32 +1,22 @@
 "use client";
 
-import { useAdminRecordsDeleteMutation } from "../hooks/admin.queries";
-import { useAdminStore } from "../model/admin.store";
-import AdminAddRecordButton from "./admin-addrecord-button";
+import AdminAddButton from "./admin-add-button";
 import AdminDeleteButton from "./admin-delete-button";
 
-function AdminHeader() {
-  const { selectedItems } = useAdminStore();
+interface AdminHeaderProps {
+  title: "records" | "descs";
+}
 
-  const {
-    mutate: deleteRecords,
-    isPending,
-    error,
-  } = useAdminRecordsDeleteMutation();
-
-  const handleDeleteRecords = () => {
-    deleteRecords(selectedItems.map((item) => item.id));
-  };
-
+function AdminHeader({ title }: AdminHeaderProps) {
   return (
     <>
       <div className="flex items-center justify-between pt-2">
         <h1 className="flex items-center justify-start pl-2 text-lg font-bold">
-          Records
+          {title === "descs" ? "about" : title}
         </h1>
         <div className="flex items-center justify-end gap-2">
-          <AdminAddRecordButton />
-          <AdminDeleteButton onClick={handleDeleteRecords} />
+          <AdminAddButton type={title} />
+          <AdminDeleteButton type={title} />
         </div>
       </div>
     </>
