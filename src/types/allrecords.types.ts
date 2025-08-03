ec3360223ebc2e43f1cadd2f47d7388f5e2077a5
EunOh <bdohhhhh@gmail.com>
2025-08-03 14:38:54 +0900

    update: kakao 친구에게 보내기 - types

--- src/types/allrecords.types.ts
+++ src/types/allrecords.types.ts
@@ -63,3 +63,63 @@ export const inquiryFormSchema = z.object({
 });
 
 export type InquiryFormValues = z.infer<typeof inquiryFormSchema>;
+
+export interface HealthCheckResponse {
+  id: string;
+  expires_in: number;
+  app_id: number;
+}
+
+export interface RefreshTokenResponse {
+  token_type: string;
+  access_token: string;
+  id_token?: string;
+  expires_in: number;
+  refresh_token?: string;
+  refresh_token_expires_in?: number;
+}
+
+export interface MessageResponse {
+  result_code: number;
+}
+
+export interface Friend {
+  id: number;
+  uuid: string;
+  favorite: boolean;
+  profile_nickname: string;
+  profile_thumbnail_image: string;
+}
+
+export interface KakaoFriendListResponse {
+  elements: Friend[];
+  total_count: number;
+  before_url: string;
+  after_url: string;
+  favorite_count: number;
+}
+
+export type TemplateObject = globalThis.Record<
+  string,
+  string | { web_url: string }
+>;
+
+export interface MessageRequest {
+  receiver_uuids: string[];
+  template_object: TemplateObject;
+  template_id?: string;
+  template_args?: {
+    name: string;
+    email: string;
+    message: string;
+  };
+}
+
+export interface MessageResponse {
+  successful_receiver_uuids: string[];
+  failure_info: {
+    code: number;
+    msg: string;
+    receiver_uuid: string[];
+  }[];
+}
