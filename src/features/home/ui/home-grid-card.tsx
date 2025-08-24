7f234a1973a2f1f54b9e968e01ea3d0980f57c77
EunOh <bdohhhhh@gmail.com>
2025-08-24 18:30:59 +0900

    update: main 에 thumbnail 표시하도록 수정

--- src/features/home/ui/home-grid-card.tsx
+++ src/features/home/ui/home-grid-card.tsx
@@ -1,18 +1,25 @@
 "use client";
 
-import type { Record } from "@/types/allrecords.types";
 import Link from "next/link";
+import type { Record } from "@/types/allrecords.types";
 
 interface HomeGridCardProps {
   record: Record;
 }
 
 function HomeGridCard({ record }: HomeGridCardProps) {
+  if (!record.thumbnail) return null;
+
   return (
     <Link href={`/${record.slug}`}>
-      <div className="rounded-lg bg-green-500 px-2 py-12">
-        <h2>{record.title}</h2>
-        <p>{record.slug}</p>
+      <div className="relative rounded-lg px-2 py-12">
+        <img
+          src={record.thumbnail}
+          alt={record.title}
+          className="absolute inset-0 h-full w-full rounded-md object-contain"
+        />
+        {/* <h2>{record.title}</h2>
+        <p>{record.slug}</p> */}
       </div>
     </Link>
   );
