a120874e31d8853cbf77675e62574b5abb625c00
EunOh <bdohhhhh@gmail.com>
2025-08-30 13:54:22 +0900

    update: floating 설정

--- src/features/home/ui/home-floating.tsx
+++ src/features/home/ui/home-floating.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 function HomeFloating() {
   return (
     <div className="fixed right-2 bottom-0">
+      <Link href="/withcat">
+        <Button
+          type="button"
+          className="h-36 w-36 cursor-pointer bg-transparent p-0 shadow-none hover:bg-transparent"
+        >
+          <div className="h-full w-full bg-[url('/main-cat.webp')] bg-center bg-contain" />
+        </Button>
+      </Link>
       <Link href="/about">
         <Button
           type="button"
-          className="cursor-pointer bg-transparent p-0 hover:bg-transparent"
+          className="h-36 w-36 cursor-pointer bg-transparent p-0 shadow-none hover:bg-transparent"
         >
-          <div className="h-14 w-14 rounded-full bg-blue-500 hover:bg-blue-800" />
+          <div className="h-full w-full bg-[url('/main-me.webp')] bg-center bg-contain" />
         </Button>
       </Link>
     </div>
--- src/templates/home-template.tsx
+++ src/templates/home-template.tsx
@@ -4,7 +4,7 @@ import { HomeFloating, HomeGrid } from "@/features/home";
 
 function HomeTemplate() {
   return (
-    <section className="flex h-svh flex-col">
+    <section className="flex flex-col">
       <div className="flex flex-col gap-4 pb-4">
         <h1 className="font-bold text-[calc(36/640*100svw)] sm:text-4xl">
           allrecords.me
