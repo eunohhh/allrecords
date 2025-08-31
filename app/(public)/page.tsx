46fa76a1ba3d3bd67f59fcdb71993b4d832aed28
EunOh <bdohhhhh@gmail.com>
2025-08-31 15:26:48 +0900

    update: 메인 프리페치 적용

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,5 +1,30 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import {
+  QUERY_KEY_ALL,
+  QUERY_KEY_RECORDS,
+} from "@/constants/allrecords.consts";
+import { getAllRecordsServer } from "@/features/home/apis/home.server-apis";
 import HomeTemplate from "@/templates/home-template";
 
-export default function Home() {
-  return <HomeTemplate />;
+async function PublicPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: [QUERY_KEY_RECORDS, QUERY_KEY_ALL],
+    queryFn: () => getAllRecordsServer(),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <HomeTemplate />
+    </HydrationBoundary>
+  );
 }
+
+export default PublicPage;
