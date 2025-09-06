ca7de9a8ae6f70184f8811e49507b877c9ec67d9
EunOh <bdohhhhh@gmail.com>
2025-09-06 14:52:01 +0900

    update: floating 수정

--- src/features/home/ui/home-floating.tsx
+++ src/features/home/ui/home-floating.tsx
@@ -5,21 +5,24 @@ import { Button } from "@/components/ui/button";
 
 function HomeFloating() {
   return (
-    <div className="fixed right-2 bottom-0">
-      <Link href="/withcat">
+    <div className="fixed right-2 bottom-1 flex flex-row items-center">
+      <Link
+        href="/withcat"
+        className="relative aspect-[173/135] h-24 w-22 sm:h-36 sm:w-32"
+      >
         <Button
           type="button"
-          className="h-24 w-24 cursor-pointer bg-transparent p-0 shadow-none hover:bg-transparent sm:h-36 sm:w-36"
+          className="flex aspect-[173/135] h-full w-full cursor-pointer items-end bg-transparent p-0 shadow-none hover:bg-transparent"
         >
-          <div className="h-full w-full bg-[url('/main-cat.webp')] bg-center bg-contain" />
+          <div className="relative bottom-1 aspect-[173/135] w-full bg-[url('/main-cat-2.webp')] bg-center bg-contain bg-no-repeat" />
         </Button>
       </Link>
-      <Link href="/about">
+      <Link href="/about" className="relative h-full w-full">
         <Button
           type="button"
           className="h-24 w-24 cursor-pointer bg-transparent p-0 shadow-none hover:bg-transparent sm:h-36 sm:w-36"
         >
-          <div className="h-full w-full bg-[url('/main-me.webp')] bg-center bg-contain" />
+          <div className="h-full w-full bg-[url('/main-me.webp')] bg-center bg-contain bg-no-repeat" />
         </Button>
       </Link>
     </div>
